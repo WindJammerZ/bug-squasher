99a151b743a96d5efcb12a61b9840b177bf97bba
Jonathan Kent <jkent355@yahoo.com>
2020-01-29 14:20:50 -0500

    tracing db connect 2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+console.log(`MongoDB URI: ${process.env.MONGODB_URI}`)
+
 mongoose.connect(process.env.MONGODB_URI, {
         useCreateIndex: true,
         useFindAndModify: false,
